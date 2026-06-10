0d1046393ede0e64615c84eea15eb8c678e4656f
Perceptronic-Systems <andrewkirk006@gmail.com>
2026-06-10 09:00:58 -0600

    Added docker compose to setup ollama as well

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-ollama serve &
-
-echo "Waiting for Ollama server to start..."
-while ! curl -s http://127.0.0.1:11434/api/tags > /dev/null; do
-    sleep 1
-done
-
 echo "Starting Omnichat backend..."
 python backend/main.py
